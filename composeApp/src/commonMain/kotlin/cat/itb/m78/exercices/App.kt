130a5af10c9519740ee622cd7ed7b2c21e662ff3
0ri6 <oriol.font.7e8@itb.cat>
2025-01-29 13:05:20 +0100

    29-1-25

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/App.kt
@@ -2,16 +2,7 @@ package cat.itb.m78.exercices
 
 
 import androidx.compose.runtime.*
-import cat.itb.m78.exercices.ExState.GoodMaN
-import cat.itb.m78.exercices.ExState.HelloScreen
-import cat.itb.m78.exercices.ExState.SecretNumber
-import cat.itb.m78.exercices.ExState.gambling
-import cat.itb.m78.exercices.ExStateless.Contact
-import cat.itb.m78.exercices.ExStateless.Resource
-import cat.itb.m78.exercices.ExStateless.Welcome
-import cat.itb.m78.exercices.ExViewModel.CounterStrike
-import cat.itb.m78.exercices.Navigation.ManualNav
-import cat.itb.m78.exercices.Navigation.Screen1
+import cat.itb.m78.exercices.ExNavigation.ManualNav
 import cat.itb.m78.exercices.theme.AppTheme
 
 @Composable
--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/ExNavigation/Ex2LibNav.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/ExNavigation/Ex2LibNav.kt
@@ -1,4 +1,4 @@
-package cat.itb.m78.exercices.Navigation
+package cat.itb.m78.exercices.ExNavigation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -7,57 +7,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-
-sealed interface Screen {
-    data object MainScreen : Screen
-    data object Screen1 : Screen
-    data object Screen2 : Screen
-    data class Screen3(val message: String) : Screen
-}
-
-private class ManualNavAppViewModel : ViewModel() {
-    val currentScreen = mutableStateOf<Screen>(Screen.MainScreen)
-    fun navigateTo(screen: Screen) {
-        currentScreen.value = screen
-    }
-}
-
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import kotlinx.serialization.Serializable
 
 @Composable
-fun ManualNav() {
-    val viewModel = viewModel { ManualNavAppViewModel() }
-    val currentScreen = viewModel.currentScreen.value
-    when (currentScreen) {
-        Screen.MainScreen -> MainScreen(
-            navigateToScreen1 = { viewModel.navigateTo(Screen.Screen1) },
-            navigateToScreen2 = { viewModel.navigateTo(Screen.Screen2) },
-            navigateToScreen3 = { viewModel.navigateTo(Screen.Screen3(it)) },
-        )
-
-        is Screen.Screen1 -> Screen1(
-            navigateToScreen0 = { viewModel.navigateTo(Screen.MainScreen) }
-        )
-
-        is Screen.Screen2 -> Screen2(
-            navigateToScreen0 = { viewModel.navigateTo(Screen.MainScreen) }
-        )
-
-        is Screen.Screen3 -> Screen3(
-            navigateToScreen0 = { viewModel.navigateTo(Screen.MainScreen) },
-            currentScreen.message
-
-        )
-    }
-}
-
-@Composable
-fun MainScreen(
+fun LibNav(
     navigateToScreen1: () -> Unit,
     navigateToScreen2: () -> Unit,
     navigateToScreen3: (String) -> Unit
@@ -79,7 +39,7 @@ fun MainScreen(
 }
 
 @Composable
-fun Screen1(navigateToScreen0: () -> Unit) {
+fun ScreenN1(navigateToScreen0: () -> Unit) {
     Column(
         modifier = Modifier.fillMaxSize().background(Color.Green),
         horizontalAlignment = Alignment.End,
@@ -93,7 +53,7 @@ fun Screen1(navigateToScreen0: () -> Unit) {
 }
 
 @Composable
-fun Screen2(navigateToScreen0: () -> Unit) {
+fun ScreenN2(navigateToScreen0: () -> Unit) {
     Column(
         modifier = Modifier.fillMaxSize().background(Color.Red),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -108,7 +68,7 @@ fun Screen2(navigateToScreen0: () -> Unit) {
 
 
 @Composable
-fun Screen3(navigateToScreen0: () -> Unit, message: String) {
+fun ScreenN3(navigateToScreen0: () -> Unit, message: String) {
     Column(
         modifier = Modifier.fillMaxSize().background(Color.Blue),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -121,5 +81,32 @@ fun Screen3(navigateToScreen0: () -> Unit, message: String) {
     }
 }
 
+object Destination {
+    @Serializable
+    data object ScreenN1
 
+    @Serializable
+    data object ScreenN2
 
+    @Serializable
+    data object ScreenN3
+}
+
+@Composable
+fun LibNavScreenSample() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Destination.ScreenN1) {
+        composable<Destination.ScreenN1> {
+            ScreenN1(
+                navigateToScreen2 = { navController.navigate(Destination.ScreenN2) },
+            )
+        }
+        composable<Destination.ScreenN2> {
+            Screen2 { navController.navigate(Destination.ScreenN3(it)) }
+        }
+        composable<Destination.ScreenN3> { backStack ->
+            val message = backStack.toRoute<Destination.ScreenN3>().message
+            ScreenN3(message)
+        }
+    }
+}
