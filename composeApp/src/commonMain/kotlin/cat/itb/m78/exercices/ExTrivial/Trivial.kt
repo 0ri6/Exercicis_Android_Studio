c8df40e440355a928481bc9a29c6128a97297ceb
0ri6 <oriol.font.7e8@itb.cat>
2025-02-12 13:05:41 +0100

    6-2-25

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/ExTrivial/Trivial.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/ExTrivial/Trivial.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import kotlinx.serialization.Serializable
 import m78exercices.composeapp.generated.resources.Res
 import m78exercices.composeapp.generated.resources.trivial
@@ -62,24 +63,18 @@ fun Trivial(navigateToSettings: () -> Unit, navigateToGame: () -> Unit) {
 fun Settings(navigateToMenu: () -> Unit) {
     Column {
         Row {
-            Text("Difficulty")
+            Text("ERROR 404")
+            Button(onClick = { navigateToMenu()} ){
+                Text("Menú")
+            }
         }
 
     }
 }
 
-@Composable
-fun Answer(navigateToMenu: () -> Unit) {
-    Column {
-        Row {
-            Text("Has acertat 0/0")
-        }
 
-    }
-}
-
-@Composable
-fun Quiz(navigateToAnswer: () -> Unit) {
+//@Composable
+/*fun Quiz(navigateToAnswer: () -> Unit) {
 //    var error1 by remember { mutableStateOf(Color.Gray) }
 //    var error2 by remember { mutableStateOf(Color.Gray) }
 //    var error3 by remember { mutableStateOf(Color.Gray) }
@@ -167,7 +162,128 @@ fun Quiz(navigateToAnswer: () -> Unit) {
 //
 //            }
 */        }
+    }*/
+@Composable
+fun Quiz(navigateToAnswer: (Int) -> Unit) {
+    val preguntes = listOf(
+        Pregunta("Com es diu el professor de llenguatge de marques?", listOf("Marc", "Juan", "Mateu", "Pere"), 2),
+        Pregunta("Quina és la nota mitjana de programació?", listOf("1", "3", "5", "6"), 1),
+        Pregunta("Qui és el delegat?", listOf("Yeray", "Eudald", "Laia", "Alvaro"), 0),
+        Pregunta("Quin va ser el primer videojoc de la història?", listOf("Pong", "Space Invaders", "Tetris", "Tennis for Two"), 3),
+        Pregunta("Quina empresa va desenvolupar la consola PlayStation?", listOf("Microsoft", "Sony", "Nintendo", "Sega"), 1),
+        Pregunta("Quina és la unitat de mesura de la velocitat d'un processador?", listOf("GB", "MHz", "KB", "BMP"), 1),
+        Pregunta("Quin joc va popularitzar el gènere Battle Royale?", listOf("Fortnite", "Apex Legends", "PlayerUnknown’s Battlegrounds (PUBG)", "Counter Strike Go (CSGo)"), 2),
+        Pregunta("Quin d'aquests és un sistema operatiu?", listOf("Photoshop", "Windows", "Excel", "Firefox"), 1),
+        Pregunta("Quina és la data de llançament de la primera Nintendo Entertainment System (NES)?", listOf("1983", "1990", "1985", "1979"), 0),
+        Pregunta("Quin és el nom del creador de Minecraft?", listOf("Notch","Herobrine","John Carmack","Markus Persson"), 3),
+        Pregunta("Quina d’aquestes targetes gràfiques és de la marca Nvidia?", listOf("Radeon", "Geforce", "Intel HD Graphics", "Vega"), 1),
+        Pregunta("Quina és la màxima resolució d'alguns monitors 4K?", listOf("1920x1080", "3840x2160", "2560x1440", "1280x720"), 1),
+        Pregunta("Quin joc va ser desenvolupat per Blizzard Entertainment i és conegut per la seva batalla en línia entre herois?", listOf("World of Warcraft", "Overwatch", "Starcraft", "Diablo"), 1),
+        Pregunta("Quin joc és el pitjor optimitzat de tots?", listOf("Ark", "Garten of Banban", "The Last of Us", "Cities: Skylines 2", "Totes són correctes"), 4),
+        Pregunta("Quin és el joc més venut del món?", listOf("Tetris", "Minecraft", "GTA (Grand Thief Auto)", "Wii Sports"), 0),
+        )
+
+
+    var preguntaActualIndex by remember { mutableStateOf(0) }
+    val preguntaActual = preguntes[preguntaActualIndex]
+
+    var respostaSeleccionadaIndex by remember { mutableStateOf(-1) }
+    var esRespostaCorrecta by remember { mutableStateOf(false) }
+    var jaContestada by remember { mutableStateOf(false) }
+    var respostesCorrectes by remember { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.Yellow),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(Modifier.height(20.dp))
+        Text("Ronda actual: ${preguntaActualIndex + 1}/15")
     }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            preguntaActual.text,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Spacer(Modifier.height(50.dp))
+
+        preguntaActual.respostes.forEachIndexed { index, resposta ->
+            val estaSeleccionada = respostaSeleccionadaIndex == index
+            val colorBotó = when {
+                jaContestada && index == preguntaActual.respostacorrecta -> Color.Green
+                jaContestada && index != preguntaActual.respostacorrecta -> Color.Red
+                else -> Color.Gray
+            }
+
+            Button(
+                onClick = {
+                    respostaSeleccionadaIndex = index
+                    jaContestada = true
+                    esRespostaCorrecta = index == preguntaActual.respostacorrecta
+                    if (esRespostaCorrecta) {
+                        respostesCorrectes++
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = colorBotó)
+            ) {
+                Text(text = resposta)
+            }
+        }
+
+        Spacer(Modifier.height(20.dp))
+
+        if (jaContestada) {
+            if (preguntaActualIndex == preguntes.size - 1) {
+                Button(
+                    onClick = { navigateToAnswer(respostesCorrectes) }
+                ) {
+                    Text("Finalitzar Joc")
+                }
+            } else {
+                Button(
+                    onClick = {
+                        preguntaActualIndex++
+                        respostaSeleccionadaIndex = -1
+                        jaContestada = false
+                    }
+                ) {
+                    Text("Següent pregunta")
+                }
+            }
+        }
+    }
+}
+@Composable
+fun Answer(respostesCorrectes: Int, navigateToMenu: () -> Unit) {
+    val totalPreguntes = 15
+
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.Cyan),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            "Respostes correctes: $respostesCorrectes/$totalPreguntes",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(Modifier.height(20.dp))
+
+        Button(onClick = { navigateToMenu() }) {
+            Text("Tornar al menú")
+        }
+    }
+}
 
 
 /*@Composable
@@ -242,7 +358,8 @@ object Trivial {
     data object Quiz
 
     @Serializable
-    data object Answer
+    data class Answer(val respostesCorrectes: Int)
+
 }
 
 data class Pregunta(
@@ -269,12 +386,14 @@ fun TrivialGame() {
         }
         composable<Trivial.Quiz> {
             Quiz(
-                navigateToAnswer = { navController.navigate(Trivial.Answer) },
+                navigateToAnswer = { navController.navigate(Trivial.Answer(it)) },
             )
         }
-        composable<Trivial.Answer> {
+        composable<Trivial.Answer> { backStack ->
+            val respostesCorrectes = backStack.toRoute<Trivial.Answer>().respostesCorrectes
             Answer(
                 navigateToMenu = { navController.navigate(Trivial.Menu) },
+                respostesCorrectes = respostesCorrectes
             )
         }
     }
